f81cd907b358d1c20b4de199841c0df5c33406fc
kiadragon <sxhdragon@gamil.com>
2014-12-05 01:22:27 +0800

    聊天室传输文件前台分块传输完成 & 后台暂不可接收和读写

--- handler/broadcast.js
+++ handler/broadcast.js
@@ -1,5 +1,8 @@
 var msgHandler = require('./message');
-
+var fs = require('fs');
+var FILE_TYPE = 'FILE';
+var TEXT_TYPE = 'TEXT';
+var UPLOAD_PATH = './upload/';
 exports.handle = function(socket, message) {
 	var _source = {
 		ip: message.destination.ip,
@@ -11,6 +14,15 @@ exports.handle = function(socket, message) {
 	};
 
 	var _statusCode = 500;
+	console.log(message.data.content.content.Final);
+	switch (message.data.content.type) {
+		case FILE_TYPE:
+			fileHandler(message);
+			break;
+		case TEXT_TYPE:
+			textHandler(message);
+			break;
+	}
 
 	var response = msgHandler.packageResponseMessage(
 		_statusCode,
@@ -18,7 +30,25 @@ exports.handle = function(socket, message) {
 		_destination,
 		message.data
 	);
-
-	socket.broadcast.emit('response', response);
-	socket.emit('response', response);
+	//socket.broadcast.emit('response', response);
+	//socket.emit('response', response);
 };
+
+/**
+	*	File Handle Function
+	*	Return Response
+**/
+function fileHandler(message) {
+	// fs.writeFile(UPLOAD_PATH + message.data.content.filename, message.data.content.content,
+	// 	{encoding: 'Binary'},
+	// 	function(err, written){
+	// 	console.log(err, written);
+	// });
+	// var data = {
+	// 	fileName : message.data.content.filename
+	// };
+}
+
+function textHandler(message) {
+
+}
