664100d5dbc42cf147066e7b55b8fcbee12ac43f
kiadragon <sxhdragon@gamil.com>
2014-12-06 14:52:00 +0800

    多文件上传完成

--- handler/broadcast.js
+++ handler/broadcast.js
@@ -3,10 +3,14 @@ var fs = require('fs');
 var FILE_TYPE = 'FILE';
 var TEXT_TYPE = 'TEXT';
 var UPLOAD_PATH = './upload/';
-var writeStream = null;
-var File_End = true;
-/*	指已上传的大小	*/
-var FileUploadPionter = 0;
+/**
+ *	File 代表上传文件的信息
+ *	同时为多文件上传提供可能
+ *	File_End 代表文件是否完结
+ *	fileUploadPionter 为已经上传的内容
+ *	writeStream 是文件的流
+ **/
+var File = [];
 exports.handle = function(socket, message) {
 	var _source = {
 		ip: message.destination.ip,
@@ -21,17 +25,36 @@ exports.handle = function(socket, message) {
 	var response;
 	switch (message.data.content.type) {
 		case FILE_TYPE:
-			if (!writeStream || File_End) {
-				writeStream = fs.createWriteStream(UPLOAD_PATH + message.data.content.filename, {
-					encoding: 'Binary'
-				});
-				File_End = false;
+			/** initialize **/
+			if (!File[message.data.content.filename]) {
+				File[message.data.content.filename] = {
+					File_End: true,
+					FileUploadPionter: 0,
+					writeStream: null
+				};
+			}
+
+			if (File[message.data.content.filename].writeStream === undefined || File[message.data.content.filename].File_End) {
+				File[message.data.content.filename] = {
+					File_End: false,
+					FileUploadPionter: 0,
+					writeStream: fs.createWriteStream(UPLOAD_PATH +
+						message.data.content.filename, {
+							encoding: 'Binary'
+						})
+				};
 			}
 			response = msgHandler.packageResponseMessage(
 				_statusCode,
 				_source,
 				_destination,
-				fileHandler(message, writeStream));
+				fileHandler(message,
+					File[message.data.content.filename].writeStream));
+			if (File[message.data.content.filename].File_End) {
+				File[message.data.content.filename].writeStream.end();
+				delete File[message.data.content.filename];
+
+			}
 			break;
 		case TEXT_TYPE:
 			response = msgHandler.packageResponseMessage(
@@ -57,7 +80,6 @@ exports.handle = function(socket, message) {
  *	Return Response.data
  **/
 function fileHandler(message, writeStream) {
-	console.log(message.data.content.filesize);
 	writeStream.write(message.data.content.content.data, 'Binary', function() {
 		if (message.data.content.content.Final) {
 			writeStream.on('end', function() {
@@ -65,30 +87,23 @@ function fileHandler(message, writeStream) {
 			});
 		}
 	});
-
-	FileUploadPionter += message.data.content.content.data.length;
+	console.log(File);
+	File[message.data.content.filename].FileUploadPionter += message.data.content.content.data.length;
 	var responseData = {
 		username: message.data.username,
 		time: message.data.time,
 		content: {
 			type: FILE_TYPE,
 			content: '文件 ' + message.data.content.filename + ' 已上传 ： ' +
-			Math.floor((FileUploadPionter / message.data.content.filesize) * 100) + "%"
+				Math.floor((File[message.data.content.filename].FileUploadPionter /
+					message.data.content.filesize) * 100) + "%"
 		}
 	};
 	if (message.data.content.content.Final) {
-		FileUploadPionter = 0;
-		File_End = true;
+		File[message.data.content.filename].FileUploadPionter = 0;
+		File[message.data.content.filename].File_End = true;
 	}
 	return responseData;
-	// fs.writeFile(UPLOAD_PATH + message.data.content.filename, message.data.content.content,
-	// 	{encoding: 'Binary'},
-	// 	function(err, written){
-	// 	console.log(err, written);
-	// });
-	// var data = {
-	// 	fileName : message.data.content.filename
-	// };
 }
 
 /**
